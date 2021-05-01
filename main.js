b01f97aabb22a07d9b2b62c860a2897d5f652fa8
VTBTS <76725953+VTBTS@users.noreply.github.com>
2021-05-01 11:26:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var text = document.getElementById("ans").value;
+
+function done(){
+    window.alert(text);
+   if( ans == "a"){
+    window.alert("no!!")
+       document.getElementById("wfpp").innerHTML.visibility=visible;
+   };
+ };
